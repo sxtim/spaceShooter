92dca97bee5c44c0e36585e33ecd1053b1444781
sxtim <sxtimx@gmail.com>
2021-10-16 23:48:03 +0300

    added method run

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     //Field
     public static int WIDTH = 400;
     public static int HEIGHT = 400;
+    Thread thread = new Thread(this);//вызывает @Override метод run
 
     //Constructor
     public GamePanel(){
@@ -13,8 +14,30 @@ public class GamePanel extends JPanel {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setFocusable(true);
         requestFocus();
+        thread.start();// старт потока
 
 
     }
+    //Functions
+    @Override
+    public void run() {
+        while(true){// TODO States
+            gameUpdate();
+            gameRender();
 
+            try {
+                thread.sleep(33); //TODO FPS
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void gameUpdate(){ // обновляет состояния
+
+    }
+
+    public void gameRender(){ // обновляет картинку
+
+    }
 }
