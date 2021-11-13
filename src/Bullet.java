51cafe2c770b505a6c973c9a910a3319d03e7a84
sxtim <sxtimx@gmail.com>
2021-11-13 23:48:41 +0300

    fixed position bullets

--- src/Bullet.java
+++ src/Bullet.java
@@ -26,27 +26,31 @@ public class Bullet {
     //Constructor
     public Bullet(int type, double x, double y, double angle) {
         this.type = type;
-
-        int speed = 10;
+        this.pos.set(x, y);
+        int speed = 0;
+        r = 0;
         switch (type) {
             case TYPE_MY_BULLET:
                 imgBullet = new ImageIcon("Image/bullets/bullet4.png").getImage();
+                r = 17;
+                speed = 10;
                 break;
             case TYPE_ENEMY_BULLET:
-                speed = 2;
+                speed = 10;
+                r = 10;
                 imgBullet = new ImageIcon("Image/bullets/bullet1.png").getImage();
                 break;
         }
-        this.pos.set(x, y);
+
         //нач координаты пули - координаты героя
-        x = pos.x;
-        y = pos.y;
+        this.x = pos.x;
+        this.y = pos.y;
         distX = GamePanel.mousePos.x - x;// разница по Х от мыши до пули
         distY = y - GamePanel.mousePos.y;// разница по Y от мыши до пули
 
         dist = (Math.sqrt(distX * distX + distY * distY));//расстояние от мыши до пули
 
-        r = 2;
+
 
         deltaPos = new Point2D(speed, 0).rotate(angle);
 //        System.out.println("bullet create with angle=" + angle + " actualAngle=" + deltaPos.angle());
@@ -66,15 +70,31 @@ public class Bullet {
 
 
     public void draw(Graphics2D g){
-        Point2D nextPos = pos.copy().add(deltaPos.copy().multiple(0));
+
+//        Point2D nextPos = pos.copy().add(deltaPos.copy().multiple(0)); // not used?
         AffineTransform origForm; //создаем объект класса AffineTransform
         origForm = g.getTransform();//получаем текущее значение
         AffineTransform newForm = (AffineTransform) (origForm.clone());//клонируем текущее значение
-      if(distX > 0)  newForm.rotate(Math.acos(distY/(dist)), pos.x, pos.y);//вертим полученное изображение относительно X и Y
-      if(distX < 0)  newForm.rotate(-Math.acos(distY/(dist)), pos.x, pos.y);//вертим полученное изображение относительно X и Y
+        if(distX > 0)  newForm.rotate(Math.acos(distY/(dist)), pos.x, pos.y);//вертим полученное изображение относительно X и Y
+        if(distX < 0)  newForm.rotate(-Math.acos(distY/(dist)), pos.x, pos.y);//вертим полученное изображение относительно X и Y
         g.setTransform(newForm);//ставим трансформированное изображение
+        g.drawImage(imgBullet, (int) pos.x - r, (int) pos.y - r, null);//рисуем картинку
         g.setTransform(origForm);//возвращаем старое значение
-        g.drawImage(imgBullet, (int) nextPos.x - 4* r, (int) nextPos.y - 3 *r, null);
+//        g.drawImage(imgBullet, (int) nextPos.x - 4* r, (int) nextPos.y - 3 *r, null);
+
+        //сервисные линии
+//        g.setColor(Color.CYAN);
+//        g.drawOval((int) (pos.x - r), (int) (pos.y - r), r * 2, r * 2);
+
+
+
+
+//                g.setColor(color);
+//        g.fillOval((int)pos.x,(int)pos.y,r,5 * r);
+
+
+//        Point2D nextPos = pos.copy().add(deltaPos.copy().multiple(10));
+//        g.drawImage(imgBullet, (int) nextPos.x, (int) nextPos.y, null);
 
 
     }
